e50e678fc95e6d52fa357fc68c3c3c1504d0c56e
snehaladhe <snehaladhe4768@gmail.com>
2022-04-25 14:12:17 +0530

    logout functinality

--- client/src/GlobalState.js
+++ client/src/GlobalState.js
@@ -1,5 +1,6 @@
 import React, { createContext, useState,useEffect} from 'react'
 import ProductsAPI from './api/ProductsAPI'
+import UserAPI from './api/UserAPI'
 import axios from 'axios'
 
 
@@ -20,7 +21,8 @@ export const DataProvider = ({ children }) => {
     },[])
     const state = {
         token: [token, setToken],
-        productsAPI:ProductsAPI()
+        productsAPI: ProductsAPI(),
+        userAPI:UserAPI(token)
     }
     return(<GlobalState.Provider value={state}>
         {children}
