2ec6364743e59d00a4a67d2df08d17301f08f5ad
snehaladhe <snehaladhe4768@gmail.com>
2022-04-25 14:55:10 +0530

    node

--- client/src/components/mainpages/utils/productItem/BtnRender.js
+++ client/src/components/mainpages/utils/productItem/BtnRender.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-function BtnRender({product}) {
-    return (
-        <div className='row_btn'>
-                <Link id="btn_buy" to="#!"> Buy</Link>
-                <Link id="btn_view" to={`/detail/${product._id}`}> View</Link>
-            </div>
-    );
+function BtnRender({ product }) {
+  return (
+    <div className="row_btn">
+      <Link id="btn_buy" to="/cart">
+        Buy
+      </Link>
+      <Link id="btn_view" to={`/detail/${product._id}`}>
+        View
+      </Link>
+    </div>
+  );
 }
 
 export default BtnRender;
